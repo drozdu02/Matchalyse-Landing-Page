63aae342697138d8faac714245d4bfaed66cb4a1
drozdu02 <olivier.drozdowski02@gmail.com>
2026-05-24 18:40:08 +0200

    Adding example parlays

--- constants/showcase-slides.ts
+++ constants/showcase-slides.ts
@@ -7,6 +7,21 @@ export type ShowcaseSlide = {
 /** Dodaj własne pliki do public/showcase/ i zaktualizuj listę. */
 export const SHOWCASE_SLIDES: ShowcaseSlide[] = [
   {
+    id: "kupon-1",
+    src: "/showcase/kupon_1.png",
+    alt: "Trafiony kupon: Lech Poznań – Crvena Zvezda, OGC Nice – Benfica",
+  },
+  {
+    id: "kupon-2",
+    src: "/showcase/kupon_2.png",
+    alt: "Trafiony kupon UFC 303: Ortega – Lopes, Smith – Dolidze, Pereira – Prochazka",
+  },
+  {
+    id: "kupon-3",
+    src: "/showcase/kupon_3.png",
+    alt: "Trafiony kupon łączony: Djokovic Australian Open, Baron KO, Tybura – Ivanov",
+  },
+  {
     id: "analysis",
     src: "/showcase/analysis.svg",
     alt: "Wizualizacja analizy meczowej",
